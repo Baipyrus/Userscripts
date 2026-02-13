198573c0153927499b31eb07199a8c34b87bae03
Baipyrus <simon.waltemathe@gmail.com>
2026-02-13 15:57:04 +0100

    chore: unify interval handling and simple renaming

--- monkeytype-solver.user.js
+++ monkeytype-solver.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         Monkeytype typer
-// @namespace    https://eiflerstrom.de/
+// @namespace    https://baipyr.us/
 // @version      2026-02-13
 // @description  Types all the characters shown in Monkeytype
-// @author       IC3P3
+// @author       Baipyrus
 // @match        https://monkeytype.com/
-// @icon         https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fcdn-1.webcatalog.io%2Fcatalog%2Fmonkeytype%2Fmonkeytype-icon-filled.png&sp=1770987398Td33d3ec9642301f3ba897fcb24cc48676971273ff44d406426223688679cd8f2
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=monkeytype.com
 // @grant        none
 // ==/UserScript==
 
 (function () {
 	'use strict';
 
-	const interv = setInterval(() => {
-		const letters = [...document.querySelectorAll('.word.active > letter')];
-		const activeWord = letters.map((e) => e.innerText).join('');
+	setInterval(() => {
+		const letters = Array.from(document.querySelectorAll('.word.active > letter'));
+		const activeWord = letters.map((l) => l.textContent).join('');
 		const wordsInput = document.querySelector('#wordsInput');
 		if (wordsInput.value.includes(activeWord)) return;
 		wordsInput.value += activeWord;
