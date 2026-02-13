33e22426bfdfafed97997260e32750e7324afbd5
Baipyrus <simon.waltemathe@gmail.com>
2026-02-13 16:35:36 +0100

    feat: refactor and commentate binary game solver

--- binary-game-solver.user.js
+++ binary-game-solver.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Cisco Binary Game Solver
-// @namespace    https://eiflerstrom.de/
+// @namespace    https://baipyr.us/
 // @version      2026-02-13
 // @description  Solves all the tasks in Ciscos Binary game
-// @author       IC3P3
+// @author       Baipyrus
 // @match        https://learningcontent.cisco.com/games/binary/index.html
-// @icon         https://www.startpage.com/av/proxy-image?piurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F027%2F075%2F812%2Fnon_2x%2Fcisco-logo-transparent-free-png.png&sp=1770987865T1b54a25a0117341c512866926bc6810387937a99fff149f0234e6ccc2752d5fd
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cisco.com
 // @grant        none
 // ==/UserScript==
 
@@ -13,32 +13,52 @@
 	'use strict';
 
 	setInterval(() => {
-		const next = document.querySelector('div.modal-body>button');
-		if (next) {
-			next.click();
+		// Go to next level or start game button
+		const advanceLevel = document.querySelector('div.modal-body > button');
+		if (advanceLevel) {
+			advanceLevel.click();
 			return;
 		}
-		const row = document.querySelector('div.bits');
-		const children = Array.from(row.childNodes);
-		if (children[0].disabled) {
-			const binary = children.reduce((acc, bit) => (acc += bit.textContent), '');
-			const value = parseInt(binary, 2).toString().split('');
+
+		// Get first row (div element with class "bits")
+		const firstRow = document.querySelector('div.bits');
+		const bits = Array.from(firstRow.childNodes);
+
+		if (firstRow.firstChild.disabled) {
+			// Convert binary to decimal (because bit-flips are disabled)
+			// First, combine all string values from the bit elements to a binary string
+			const binaryValue = bits.reduce((accumulator, bit) => (accumulator += bit.textContent), '');
+			// Convert binary value into decimal, split by digit for easier keypad input
+			const decimalValue = parseInt(binaryValue, 2).toString().split('');
+
+			// Open decimal keypad input
 			document.querySelector('div.digits').click();
-			const buttons = Array.from(document.querySelectorAll('button.button'));
-			const confirm = buttons.filter((btn) => btn.firstChild.tagName === 'IMG')[1];
-			const numbers = buttons.filter((btn) => btn.firstChild.nodeName === '#text');
-			value.forEach((digit) => numbers[Number(digit)].click());
-			confirm.click();
+
+			// Get keypad elements for decimal input
+			const keypadButtons = Array.from(document.querySelectorAll('button.button'));
+			const [_, confirmButton] = keypadButtons.filter((btn) => btn.firstChild.tagName === 'IMG');
+			const keypadNumbers = keypadButtons.filter((btn) => btn.firstChild.nodeName === '#text');
+
+			// Enter decimal value in keypad and conform input
+			decimalValue.forEach((digit) => keypadNumbers[Number(digit)].click());
+			confirmButton.click();
 		} else {
-			children.forEach((bit) => {
+			// Convert decimal to binary (because keypad input is disabled)
+			// First, disable all bits (by setting them to '0')
+			bits.forEach((bit) => {
 				if (bit.textContent === '0') return;
 				bit.click();
 			});
-			const value = parseInt(document.querySelector('div.digits').textContent);
-			const binary = value.toString(2).padLeft(8, '0').split('');
-			binary.forEach((bit, idx) => {
+
+			// Get decimal value from element with class "digits"
+			const decimalValue = parseInt(document.querySelector('div.digits').textContent);
+			// Convert decimal value to binary, pad the string with zeroes on the left
+			const binaryValue = decimalValue.toString(2).padLeft(8, '0').split('');
+
+			// Flip all bits from input that are supposed to be ones
+			binaryValue.forEach((bit, idx) => {
 				if (bit === '0') return;
-				children[idx].click();
+				bits[idx].click();
 			});
 		}
 	}, 1500);
